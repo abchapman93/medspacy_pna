eff982af05332151e7e547e79c79a2b2ac611865
Alec Chapman <abchapman93@gmail.com>
2022-04-27 08:14:09 -0600

    add resources to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from sys import platform
 
 # read the contents of the README file
+import os
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
+
+# function to recursively get files for resourcee
+def package_files(directory):
+    paths = []
+    for (p, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join("..", p, filename))
+    return paths
+
+resource_files = package_files("./medspacy_pna/resources")
 
 setup(
     name="medspacy_pna",
@@ -20,4 +31,5 @@ setup(
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
+    package_data={"medspacy_pna": resource_files},
 )
